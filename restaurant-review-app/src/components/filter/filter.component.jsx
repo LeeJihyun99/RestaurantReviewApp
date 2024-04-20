398bda01a7e9bfc0ca0d59dde40f78eca5e9b669
lek <lek@cip.ifi.lmu.de>
2024-04-20 16:02:06 +0200

    Update sccs, decorate a bit :_>

--- restaurant-review-app/src/components/filter/filter.component.jsx
+++ restaurant-review-app/src/components/filter/filter.component.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './filter.styles.scss';
 
 const Filter = () => {
     const [cuisine, setCuisine] = useState('All');
@@ -8,15 +9,18 @@ const Filter = () => {
     };
 
     return (
-        <div>
+        <div className="filter-container">
             <h2>Filter</h2>
-            <select value={cuisine} onChange={handleChange}>
-                <option value="All">All</option>
-                <option value="Italian">Italian</option>
-                <option value="Chinese">Chinese</option>
-                <option value="Indian">Indian</option>
-                {/* Add more options as needed */}
-            </select>
+            <div className="filter-content">
+                <label htmlFor="cuisine">Cuisine: </label>
+                <select id="cuisine" value={cuisine} onChange={handleChange}>
+                    <option value="All">All</option>
+                    <option value="Italian">Italian</option>
+                    <option value="Chinese">Chinese</option>
+                    <option value="Indian">Indian</option>
+                    {/* Add more options as needed */}
+                </select>
+            </div>
         </div>
     );
 };
