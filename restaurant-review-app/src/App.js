a9406e83852ef36af2e8fd7547455298f6d07623
ljihyun99 <Ji.Lee@campus.lmu.de>
2024-04-23 15:44:28 +0200

    installed react google button and added the button element (without functionality yet)

--- restaurant-review-app/src/App.js
+++ restaurant-review-app/src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navigation from "./routes/navigation/navigation.component";
 import Home from './routes/home/home.component';
+import GoogleButton from 'react-google-button'
 
 const About = ()=> (
   <h2>This is the about page.</h2>
 )
 const Login = ()=> (
+  <div>
   <h2>This is the login page.</h2>
+  <GoogleButton/>
+  </div>
+
 )
 const App= ()=> {
   return (
