a3e73303355ce8a5841bb827bc541227aaab432e
Minh Le <moin.morning2000@gmail.com>
2024-05-31 02:07:50 +0200

    Add error handle -> if login page close -> error firebase message shown

--- restaurant-review-app/src/routes/signin/signin.component.jsx
+++ restaurant-review-app/src/routes/signin/signin.component.jsx
@@ -1,20 +1,37 @@
+import { useState } from 'react';
 import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 import GoogleButton from "react-google-button";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
-const SignIn = ()=> {
-    const logGoogleUser = async ()=> {
-        const {user} = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
+const SignIn = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
+
+    const logGoogleUser = async () => {
+        try {
+            setIsLoading(true);
+            const { user } = await signInWithGooglePopup();
+            await createUserDocumentFromAuth(user);
+            setError('');
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setIsLoading(false);
+        }
     }
 
-    return(
+    return (
         <div>
             <h2>Sign-in Page</h2>
-            <GoogleButton onClick={logGoogleUser}/>
-            <SignUpForm/>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                <GoogleButton onClick={logGoogleUser} />
+            )}
+            <SignUpForm />
         </div>
-    )
+    );
 }
 
 export default SignIn;
